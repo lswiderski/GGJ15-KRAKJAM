19e347033d2d1b5f5ad62b782318a67ae7aaccdd
Neufrin <neufrin@gmail.com>
2015-01-24 13:02:58 +0100

    3x dooroverlay

--- WhatDoWeDoNow/WhatDoWeDoNow/WhatDoWeDoNow/Screens/MainScreen/MainGameScreen.cs
+++ WhatDoWeDoNow/WhatDoWeDoNow/WhatDoWeDoNow/Screens/MainScreen/MainGameScreen.cs
@@ -99,11 +99,29 @@ namespace WhatDoWeDoNow.Screens.MainScreen
                 }
             }
             var dr = doors.ElementAt(0);
+            var dr2 = doors.ElementAt(1);
+            var dr3 = doors.ElementAt(2);
             if (player.Position.Y> dr.BoundingBox.Y &&
                 player.Position.Y< dr.BoundingBox.Y + dr.BoundingBox.Height &&
                 player.Position.X< dr.BoundingBox.X + dr.BoundingBox.Width )
             {
                 overlaylevel = 1- (player.Position.X- dr.BoundingBox.X)/30;
+            }
+            else if (player.Position.Y> dr3.BoundingBox.Y &&
+                player.Position.Y< dr3.BoundingBox.Y + dr3.BoundingBox.Height &&
+                player.Position.X+player.BoundingBox.Width> dr3.BoundingBox.X)
+            {
+                overlaylevel = (player.Position.X + player.BoundingBox.Width - dr3.BoundingBox.X) / 30;
+            }
+            else if (player.Position.X> dr2.BoundingBox.X &&
+                player.Position.X< dr2.BoundingBox.X + dr2.BoundingBox.Width &&
+                player.Position.Y< dr2.BoundingBox.Y + dr2.BoundingBox.Height)
+            {
+                overlaylevel = 1 - (player.Position.Y - dr2.BoundingBox.Y) / 30;
+            }
+            else
+            {
+                overlaylevel = 0;
             }
             // Check if n is pressed and go to screen2
             if (Keyboard.GetState().IsKeyDown(Keys.N))
